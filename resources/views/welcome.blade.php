f7435d120a378002b825b1b9ce21ebfb99c01f90
zeinab yari <zeiinabyarii@gmail.com>
2024-01-10 17:35:02 +0330

    change welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>zedd dev</title>
+        <title>zeddd dev</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
